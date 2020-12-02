4ad8d8b3e13d0f5d557e884c6b70f3d1dee72db6
Chathuranga Wijesinghe <chathuranga.w@eyepax.com>
2020-12-02 20:37:43 +0800

    init

---
+++ apps/navbar/src/app/primary-nav/primary-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'single-spa-angular-test-primary-nav',
+  templateUrl: './primary-nav.component.html',
+  styleUrls: ['./primary-nav.component.css']
+})
+export class PrimaryNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
